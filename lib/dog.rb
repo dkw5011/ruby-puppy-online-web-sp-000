ab6480c6fb71dac6b525dda4a6c0a310cb521246
Danielle Bergbower <dkw5011@gmail.com>
2019-09-02 16:28:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+  @@all = []
+  @@print_all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+  puts  @@all.to_a
+end
+
+end
+
+
+
+  
+  
+    
